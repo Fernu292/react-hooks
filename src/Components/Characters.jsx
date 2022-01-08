5a01df7d200564d195c39f03e920264976260c63
Luis Fernando <fernu292@gmail.com>
2022-01-07 19:13:46 -0600

    Agregando useCallback

--- src/Components/Characters.jsx
+++ src/Components/Characters.jsx
@@ -1,89 +1,96 @@
-import React, {useReducer, useState, useMemo, useRef, useCallback} from 'react';
-import Character from './Character';
-import '../styles/characters.scss';
+import React, {
+  useReducer,
+  useState,
+  useMemo,
+  useRef,
+  useCallback,
+} from "react";
+import Character from "./Character";
+import Search from "./Search";
+import "../styles/characters.scss";
 
 //Creano state inicial
 const initialState = {
-    favorites : [],
+  favorites: [],
 };
 
 //Creando la logica del reducer con un switch
-const favoriteReducer = (state, action)=>{
-    switch(action.type){
-        case "ADD_TO_FAVORITE":
-            return {
-                ...state,
-                favorites: [...state.favorites, action.payload]
-            }
-        default: 
-            return state;
-    }
-}
+const favoriteReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_FAVORITE":
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload],
+      };
+    default:
+      return state;
+  }
+};
 
-const Characters = ({characters}) => {
+const Characters = ({ characters }) => {
+  //uso de reducer: funcion, state
+  const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
 
-    //uso de reducer: funcion, state
-    const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
+  const [search, setSearch] = useState("");
+  const searchInput = useRef(null);
 
-    const [search, setSearch] = useState('');
-    const searchInput = useRef(null);
+  const handleClick = (favorite) => {
+    dispatch({ type: "ADD_TO_FAVORITE", payload: favorite });
+  };
 
-    const handleClick = (favorite)=>{
-        dispatch({type: "ADD_TO_FAVORITE", payload: favorite});
-    }
+  //Sin useCallback
+  // const handleSearch = () =>{
+  //     setSearch(searchInput.current.value);
+  // }
 
-    const handleSearch = () =>{
-        setSearch(searchInput.current.value);
-    }
+  const handleSearch = useCallback(() => {
+    setSearch(searchInput.current.value);
+  }, []);
 
-    //Forma de filtro sin useMemo
-    // const filteredUsers = characters.filter(user =>{
-    //      return user.name.toLowerCase().includes(search.toLowerCase());
-    //  });
+  //Forma de filtro sin useMemo
+  // const filteredUsers = characters.filter(user =>{
+  //      return user.name.toLowerCase().includes(search.toLowerCase());
+  //  });
 
+  //Funcion usada con useMemo para evitar reRenders
+  const filteredUsers = useMemo(
+    () =>
+      characters.filter((user) => {
+        return user.name.toLowerCase().includes(search.toLowerCase());
+      }),
+    [characters, search]
+  );
 
-    //Funcion usada con useMemo para evitar reRenders
-    const filteredUsers = useMemo( ()=> (
-        characters.filter(user =>{
-           return user.name.toLowerCase().includes(search.toLowerCase());
-        })
-    ), [characters, search]);
+  return (
+    <>
+      <Search
+        search={search}
+        searchInput={searchInput}
+        handleSearch={handleSearch}
+      />
 
-    return ( 
-        <>
-            <div>
-                <input 
-                    type='text' 
-                    value={search} 
-                    onChange={handleSearch}
-                    ref={searchInput}
-                />
-            </div>
+      <div className="favorites">
+        <h3>Favorites characters: </h3>
+        {favorites.favorites.map((favorite) => (
+          <li key={favorite.id}>{favorite.name}</li>
+        ))}
+      </div>
 
-            <div className='favorites'>
-                <h3>Favorites characters: </h3>
-                {favorites.favorites.map(favorite =>(
-                    <li key={favorite.id}>{favorite.name}</li>
-                ))}
-            </div>
-                    
-            <section className='personajes'>
-            
-                {
-                    filteredUsers ? 
-                        filteredUsers.map( character =>(
-                            <Character 
-                                key ={character.id}
-                                character={character}
-                                handleClick = {handleClick}
-                            />
-                        ))
-                    : <p>Loading ...</p>
-                }
-            </section>
-        </>
-       
-    );
-}
- 
+      <section className="personajes">
+        {filteredUsers ? (
+          filteredUsers.map((character) => (
+            <Character
+              key={character.id}
+              character={character}
+              handleClick={handleClick}
+            />
+          ))
+        ) : (
+          <p>Loading ...</p>
+        )}
+      </section>
+    </>
+  );
+};
+
 export default Characters;

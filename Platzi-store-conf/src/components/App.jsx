f648f24667e2dc868b09377f7094386de6bd4801
Luis Fernando <fernu292@gmail.com>
2022-01-08 20:59:49 -0600

    Agregando prettier y eslint

--- Platzi-store-conf/src/components/App.jsx
+++ Platzi-store-conf/src/components/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../styles/App.css';
 
 const App = () => {
-    return ( 
-        <>
-            <h1>Platzi Conf</h1>
-        </>
-    );
-}
- 
+  return (
+    <>
+      <h1>Platzi Conf</h1>
+    </>
+  );
+};
+
 export default App;
--- Platzi-store-conf/webpack.config.js
+++ Platzi-store-conf/webpack.config.js
@@ -1,46 +1,54 @@
 const path = require('path');
-const HTMLWebpackPlugin = require("html-webpack-plugin");
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
-    },
-    resolve: {
-        extensions: [".js",".jsx"]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use:{
-                    loader: "babel-loader"
-                }
-            }, 
-            {
-                test: /\.html$/,
-                use: {
-                    loader: "html-loader"
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: "./public/index.html",
-            filename: "index.html",
-            inject: true
-        })
-    ],
-    devServer: {
-        static: {
-            publicPath: path.join(__dirname, "dist")
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-        port: 3000,
-        compress: true,
-        open: true 
-    }
-}
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+      inject: true,
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
+    }),
+  ],
+  devServer: {
+    static: {
+      publicPath: path.join(__dirname, 'dist'),
+    },
+    port: 3000,
+    compress: true,
+    open: true,
+  },
+};

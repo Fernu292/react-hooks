2a8e62b787090119cd7ce4f8f579dabcff6d6737
Luis Fernando <fernu292@gmail.com>
2022-01-15 17:09:51 -0600

    Agregando Google Maps API

--- Platzi-store-conf/src/components/Map.jsx
+++ Platzi-store-conf/src/components/Map.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, {useCallback, useState} from 'react';
+import {GoogleMap, LoadScript, Marker, useJsApiLoader} from "@react-google-maps/api";
+
 
 const Map = () => {
-    return (
-        <div>
+
+    const containerStyle = {
+        heigth: "400px",
+        width: "400px"
+    }
+
+    const defaultCenter = {
+        lat: -3.745,
+        lng: -38.523
+    }
+
+    const {isLoaded} = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyBKvkZ_1Pes9FksGIGWKMcjfMmqRCXeL9g"
+    })
+
+    const [map, setMap] = useState(null);
+
+    const onLoad = useCallback( (map)=>{
+        const bounds = new window.google.maps.LatLngBounds();
+        map.fitBounds(bounds);
+        setMap(map);
+    },[])
+
+    const onUnmount = useCallback( map =>{
+        setMap(null);
+    }, [])
+
+    return isLoaded ? (
             
-        </div>
-    )
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            zoom={9}
+            center={defaultCenter}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+        >
+            <Marker position={defaultCenter} />
+        </GoogleMap>
+    ) : <></>
 }
 
 export default Map;
